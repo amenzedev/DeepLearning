d4085087a49e4c6343fc5b106b1a31d3f2edb544
AmanuelGhebreyohannes <amageb2014@gmail.com>
2022-05-31 19:00:27 -0400

    anotation to tflite maker format changer python program

---
+++ Dataset_Collection_Helpers/Anotation_to_tfmaker_Object_detector_format.py
@@ -0,0 +1,48 @@
+import os
+import json
+
+validation_set = [1,23,31,35,39,40,56,60,61,80] #10% validation from 100 image dataset
+testing_set = [29,36,39,52,54,70,82,85,90,92] #10% testing from 100 image dataset
+
+output_file_name = "bounding_boxes.txt"
+out_file = open(output_file_name, "w")
+arr = os.listdir()
+file_counter = 0
+for file in arr:
+    
+    if "reformat_barrel.py" in file or ".jpg" in file or ".png" in file or ".txt" in file or ".jpeg" in file:
+        #print(file)
+        arr.remove(file)
+    elif ".json" in file:
+        #print(file)
+        f = open(file)
+        data = json.load(f)
+        # #print(data['asset'])
+        print(data['asset']['name'])
+        image_file = data['asset']['name']
+        # info_name = data['asset']['name'].replace(".jpg",".txt")
+        # info_name = info_name.replace(".jpeg",".txt")
+        # info_name = info_name.replace(".png",".txt")
+        # info_name = info_name.replace(".gif",".txt")
+
+        # print(info_name)
+        for i in range (len(data['regions'])):
+            left = data['regions'][i]['boundingBox']['left']
+            top = data['regions'][i]['boundingBox']['top']
+            right =  data['regions'][i]['boundingBox']['width']+left
+            bottom = data['regions'][i]['boundingBox']['height']+top
+            #print((left,top),(right,bottom))
+            
+            if(file_counter in validation_set):                
+                string_out = "VALIDATE,door_dataset/"+image_file+",door,"+str(left)+","+str(top)+","+str(right)+","+str(bottom)+"\n"
+            elif(file_counter in testing_set):                
+                string_out = "TEST,door_dataset/"+image_file+",door,"+str(left)+","+str(top)+","+str(right)+","+str(bottom)+"\n"
+            else:                
+                string_out = "TRAIN,door_dataset/"+image_file+",door,"+str(left)+","+str(top)+","+str(right)+","+str(bottom)+"\n"
+        
+            out_file.write(string_out)
+            print((left,top),(right,bottom))
+        f.close()
+    file_counter+=1
+out_file.close()
+        
